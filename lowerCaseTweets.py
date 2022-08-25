fc6d531e08712fd240f7c56eb7ecf70a7add4915
Connor Preston <connor@prestonweb.com>
2022-08-24 22:15:01 -0500

    Data prep pipeline: remove RTs, punct, nums, links, convert to lower

--- lowerCaseTweets.py
+++ lowerCaseTweets.py
@@ -4,4 +4,5 @@ def tweets_to_lower_case(tweetDataSet: DataFrame) -> DataFrame:
     tweets = tweetDataSet.loc[:, 'Tweet']
     tweets = tweets.str.lower()
 
-    return tweetDataSet.update(tweets)
+    tweetDataSet.update(tweets)
+    return tweetDataSet
--- prepare_pipeline.py
+++ prepare_pipeline.py
@@ -1,6 +1,6 @@
 import pandas as pd
 df = pd.read_csv('ExtractedTweets.csv',encoding="utf-8",dtype=str)
-print("Imported Tweet Data")
+print("Imported tweet data")
 
 import removeRT
 df = removeRT.remove_rt(df)
@@ -8,7 +8,11 @@ print("Removed retweets")
 
 import cleanText
 df = cleanText.clean_text(df)
-print("removed punctuation, numbers, links")
+print("Removed punctuation, numbers, links")
+
+import lowerCaseTweets
+df = lowerCaseTweets.tweets_to_lower_case(df)
+print("Converted to lowercase")
 
 # print(df["Tweet"][25])
 df.to_csv('cleanData.csv')
