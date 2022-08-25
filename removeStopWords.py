a07509584a5b1e460b5fbce5dc52a4d328b31b85
CodeforEvolution <secundaja@gmail.com>
2022-08-25 13:47:32 -0500

    Data Cleaning: Add stop word removal stage

---
+++ removeStopWords.py
@@ -0,0 +1,28 @@
+from pandas import DataFrame, Series
+from pandas import concat, read_csv, read_excel
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk import word_tokenize
+
+def remove_stop_words(inputData: DataFrame) -> DataFrame:
+    # Build Series with all words we want to remove from our Tweets
+    wordsToRemove = read_excel('contractions.xlsx', dtype=str).squeeze('columns')
+    wordsToRemove = concat([Series(stopwords.words('english')), wordsToRemove])
+
+    # Obtain the column of the Twitter data with the tweets.
+    tweetSeries = inputData.loc[:, 'Tweet']
+
+    # Loop through list of tweets.
+    for index, tweet in enumerate(tweetSeries.to_list()):
+        # For each tweet, tokenize, remove stop words, and overwrite old tweet with filtered tweet in list.
+        tokenizedTweet = word_tokenize(tweet)
+        filteredTweet = [word for word in tokenizedTweet if not word in wordsToRemove.to_list()]
+        tweetSeries[index] = ' '.join(filteredTweet)
+
+    # Update the passed in Twitter data with the column of filtered tweets.
+    inputData.update(tweetSeries)
+    return inputData
